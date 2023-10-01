c95725edc7e0aa7d01ebf040357f7b0a34fea034
Randy Fong <randyfong@aol.com>
2023-10-01 14:09:06 -0700

    Commit using Note Field in Calendar to prompt ChatGPT for Life Hackathon.

--- MyCalendarAi/ContentView.swift
+++ MyCalendarAi/ContentView.swift
@@ -6,17 +6,102 @@
 //
 
 import SwiftUI
+import EventKit
+import OpenAIKit
+
+struct EventCalendar: Identifiable {
+    let id = UUID()
+    let title: String
+    let location: String
+    let startDate: Date
+    let endDate: Date
+    let notes: String
+    let url: URL?
+}
 
 struct ContentView: View {
+    @State var event: String = ""
+    @State var location: String = ""
+    @State var store = EKEventStore()
+    @State var source = EKSource()
+    @State var calendarEvents = [EventCalendar]()
+    @State var notes: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Form {
+            TextField("Event", text: $event)
+            TextField("Location", text: $location, axis: .vertical)
+            TextField("Notes", text: $notes, axis: .vertical)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            }
+        .onAppear {
+            Task {
+                let isAuthorized = try await requestAccess()
+                if isAuthorized {
+                    await getEvents()
+                }
+            }
         }
-        .padding()
     }
+    
+    func requestAccess(to: EKEntityType = .event) async throws -> Bool{
+        return try await withCheckedThrowingContinuation { continuation in
+            store.requestAccess(to: to) { granted, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume(returning: granted)
+                }
+            }
+        }
+    }
+    
+    func getEvents() async {
+        let calendars = store.calendars(for: .event)
+        
+        for calendar in calendars {
+            if calendar.title == "Calendar" {
+                let oneMonthAgo = Date(timeIntervalSinceNow: 0*20*3600)
+                let oneMonthAfter = Date(timeIntervalSinceNow: 7*24*3600)
+                let predicate =  store.predicateForEvents(withStart: oneMonthAgo, end: oneMonthAfter, calendars: [calendar])
+                
+                let events = store.events(matching: predicate)
+                calendarEvents.removeAll()
+                events.forEach {
+                    if $0.title == "Lunch" {
+                        let event = EventCalendar(title: $0.title, location: $0.location ?? "", startDate: $0.startDate, endDate: $0.endDate, notes: $0.notes ?? "", url: $0.url)
+                        calendarEvents.append(event)
+                        // print(calendarEvents.last)
+                    }
+                }
+                if let lastCalendarEvent = calendarEvents.last {
+                    event = lastCalendarEvent.title
+                    location = lastCalendarEvent.location
+                    try? await sendPrompt(lastCalendarEvent)
+                }
+ 
+            }
+        }
+    }
+        
+    func sendPrompt(_ event: EventCalendar) async throws {
+        let openAI = OpenAIKit(apiToken: "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")
+        
+        
+        let prompt = event.notes
+        let result = await openAI.sendCompletion(prompt: prompt, model: .gptV3_5(.davinciText003), maxTokens: 2048)
+
+        switch result {
+        case .success(let aiResult):
+            if let text = aiResult.choices.first?.text {
+                notes = text
+                // print("\(text)")
+            }
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
