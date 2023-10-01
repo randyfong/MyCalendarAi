8e59bc7e03225965917a76b1f589d93ac3f86103
Randy Fong <randyfong@aol.com>
2023-10-01 13:07:53 -0700

    Initial Commit

---
+++ MyCalendarAi/MyCalendarAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalendarAiApp.swift
+//  MyCalendarAi
+//
+//  Created by Randy Fong on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MyCalendarAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
